25cbfe827353173211716cf0569465c148179e98
arturoceballos <aceballos118@gmail.com>
2016-01-16 16:30:09 -0800

    Removed parentheses since we only have one argument in function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render () {
-        return <input onChange={(event) => console.log(event.target.value)} />;
+        return <input onChange={event => console.log(event.target.value)} />;
     }
 }
 
