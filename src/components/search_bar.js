077cd67b61aa740f17d005a102bccac4555cb862
arturoceballos <aceballos118@gmail.com>
2016-01-16 17:10:51 -0800

    Put the searchbar in a div and am now rendering what's being typed in the searchbar by referencing term in curly braces (curly braces are used whenever you want to reference javascript inside of jsx)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,12 @@ class SearchBar extends Component {
     }
 
     render () {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+                <input onChange={event => this.setState({ term: event.target.value})} />
+                Value of the input: {this.state.term}
+            </div>
+        );
     }
 }
 
