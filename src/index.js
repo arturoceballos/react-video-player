ab7f72d677ae81a94bee5aed61ed98badd0c0089
arturoceballos <aceballos118@gmail.com>
2016-01-17 23:16:03 -0800

    Created VideoList component and rendered it in the App component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCR6E9yQT_yXvf6pmTSFKg32T687Er48XM';
 
@@ -21,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList />
             </div>
         );
     }
