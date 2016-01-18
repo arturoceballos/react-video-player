48b2e0290a987019ae1b192bca3f7c053e057e3b
arturoceballos <aceballos118@gmail.com>
2016-01-18 11:38:32 -0800

    Console was throwing an error because it couldn't find the video ID since the request to the Youtube API didn't finish and the VideoDetail component tried to render something that didn't exist yet. Added a conditional that renders a div that says "Loading..." until the array of videos are received.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCR6E9yQT_yXvf6pmTSFKg32T687Er48XM';
 
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
