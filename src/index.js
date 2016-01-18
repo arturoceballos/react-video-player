fbf0f01b17dfe81f02763b1963f0cd3d49406390
arturoceballos <aceballos118@gmail.com>
2016-01-17 23:04:57 -0800

    imported a Youtube search library that'll make searching for videos easy. Refactored functional component into a class-based component because we will need to update the state if we want to display new videos based on search terms. Removed comments because I've got this shieeeeet.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCR6E9yQT_yXvf6pmTSFKg32T687Er48XM';
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'snowboarding'}, (videos) => {
+            this.setState({videos: videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
