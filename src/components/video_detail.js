5fddabcb98a2202daee832e85d2b4eef22fc18ef
arturoceballos <aceballos118@gmail.com>
2016-01-18 11:26:11 -0800

    Using ES6 string interpolation to create the embed url instead of the old nasty syntax

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoDetail = ({video}) => {
     const videoId = video.id.videoId;
-    const url = 'https://www.youtube.com/embed/' + videoId;
+    const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
         <div className="video-detail col-md-8">
